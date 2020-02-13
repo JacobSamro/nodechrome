92a061e0c4908920262c8cb953183a4c95ae69fb
Jacob Samro <JacobSamro@users.noreply.github.com>
2020-02-13 15:13:55 +0530

    version 1.0.1

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSION=1.0.0
+VERSION=1.0.1
 docker build . -t jacobsamro/nodecbse:$VERSION
 docker tag jacobsamro/nodecbse:$VERSION jacobsamro/nodecbse:latest
 docker push jacobsamro/nodecbse:$VERSION
